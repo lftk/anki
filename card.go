0fb630970d5a1cc440cfbaa0ff507326e20ada61
lftk <dev.zhoujun@gmail.com>
2025-08-22 21:37:17 +0800

    remove direct card manipulation methods from Collection

--- card.go
+++ card.go
@@ -1,7 +1,6 @@
 package anki
 
 import (
-	"database/sql"
 	"iter"
 	"time"
 )
@@ -55,22 +54,6 @@ func (c *Collection) GetCard(id int64) (*Card, error) {
 	return sqlGet(c.db, scanCard, query, id)
 }
 
-func (c *Collection) AddCard(card *Card) error {
-	return sqlTransact(c.db, func(tx *sql.Tx) error {
-		card.Modified = time.Now()
-		card.USN = -1
-
-		res, err := tx.Exec(`INSERT INTO cards (nid, did, ord, mod, usn, type, queue, due, ivl, factor, reps, lapses, left, odue, odid, flags, data) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-			card.NoteID, card.DeckID, card.Ordinal, card.Modified.UnixMilli(), card.USN, card.Type, card.Queue, card.Due, card.Interval, card.Factor, card.Repetitions, card.Lapses, card.Left, card.OriginalDue, card.OriginalDeckID, card.Flags, card.Data)
-		if err != nil {
-			return err
-		}
-
-		card.ID, err = res.LastInsertId()
-		return err
-	})
-}
-
 func addCard(e sqlExecer, card *Card) error {
 	id := card.ID
 	if id == 0 {
@@ -101,21 +84,6 @@ func addCard(e sqlExecer, card *Card) error {
 		card.ID = id
 	}
 	return err
-}
-
-func (c *Collection) UpdateCard(card *Card) error {
-	return sqlTransact(c.db, func(tx *sql.Tx) error {
-		card.Modified = time.Now()
-		card.USN = -1
-
-		_, err := tx.Exec(`UPDATE cards SET nid = ?, did = ?, ord = ?, mod = ?, usn = ?, type = ?, queue = ?, due = ?, ivl = ?, factor = ?, reps = ?, lapses = ?, left = ?, odue = ?, odid = ?, flags = ?, data = ? WHERE id = ?`,
-			card.NoteID, card.DeckID, card.Ordinal, card.Modified.UnixMilli(), card.USN, card.Type, card.Queue, card.Due, card.Interval, card.Factor, card.Repetitions, card.Lapses, card.Left, card.OriginalDue, card.OriginalDeckID, card.Flags, card.Data, card.ID)
-		return err
-	})
-}
-
-func (c *Collection) DeleteCard(id int64) error {
-	return sqlExecute(c.db, "DELETE FROM cards WHERE id = ?", id)
 }
 
 func (c *Collection) ListCards() iter.Seq2[*Card, error] {
